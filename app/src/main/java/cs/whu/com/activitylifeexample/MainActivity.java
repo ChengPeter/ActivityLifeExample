1980b4ee876510f255deeacd19f047057a4dadbf
714905267@qq.com <714905267@qq.com>
2017-05-18 21:32:50 +0800

    first commit

---
+++ app/src/main/java/cs/whu/com/activitylifeexample/MainActivity.java
@@ -0,0 +1,13 @@
+package cs.whu.com.activitylifeexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
